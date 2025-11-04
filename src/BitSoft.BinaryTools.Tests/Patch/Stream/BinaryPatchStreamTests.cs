76d9e2e1faf90ca0d37ef352320787190d307bd7
bitc0der <59016822+bitc0der@users.noreply.github.com>
2025-11-04 12:41:45 +0700

    Add test cases for various segment sizes

--- src/BitSoft.BinaryTools.Tests/Patch/Stream/BinaryPatchStreamTests.cs
+++ src/BitSoft.BinaryTools.Tests/Patch/Stream/BinaryPatchStreamTests.cs
@@ -10,13 +10,17 @@ public class BinaryPatchStreamTests
 {
     private static IEnumerable<TestCaseData> GetTestCaseData()
     {
-        yield return new TestCaseData(new byte[] { 0x0, 0x1, 0x2 }, new byte[] { 0x0, 0x1, 0x3 });
-        yield return new TestCaseData(new byte[] { 0x0, 0x1, 0x2 }, new byte[] { 0x0, 0x3 });
-        yield return new TestCaseData(new byte[] { 0x0, 0x1 }, new byte[] { 0x0, 0x1, 0x3 });
+        yield return new TestCaseData(new byte[] { 0x0, 0x1, 0x2 }, new byte[] { 0x0, 0x1, 0x3 }, 1);
+        yield return new TestCaseData(new byte[] { 0x0, 0x1, 0x2 }, new byte[] { 0x0, 0x3 }, 1);
+        yield return new TestCaseData(new byte[] { 0x0, 0x1 }, new byte[] { 0x0, 0x1, 0x3 }, 1);
+        yield return new TestCaseData(new byte[] { 0x0, 0x1, 0x2 }, new byte[] { 0x0, 0x1, 0x3 }, 2);
+        yield return new TestCaseData(new byte[] { 0x0, 0x1, 0x2 }, new byte[] { 0x0, 0x1, 0x3, 0x4 }, 2);
+        yield return new TestCaseData(new byte[] { 0x0, 0x1 }, new byte[] { 0x0, 0x1, 0x3 }, 2);
+        yield return new TestCaseData(new byte[] { 0x0, 0x1, 0x2, 0x3 }, new byte[] { 0x0, 0x1, 0x3 }, 2);
     }
-    
+
     [TestCaseSource(nameof(GetTestCaseData))]
-    public async Task Should_WriteToStream(byte[] original, byte[] modified)
+    public async Task Should_WriteToStream(byte[] original, byte[] modified, int segmentSize)
     {
         // Arrange
         using var originStream = new MemoryStream(original);
@@ -24,7 +28,7 @@ public class BinaryPatchStreamTests
         using var patchStream = new MemoryStream();
 
         // Act
-        await BinaryPatchWriter.WritePatchAsync(originStream, modifiedStream, patchStream, segmentSize: 1);
+        await BinaryPatchWriter.WritePatchAsync(originStream, modifiedStream, patchStream, segmentSize: segmentSize);
 
         // Assert
         originStream.Position = 0;
