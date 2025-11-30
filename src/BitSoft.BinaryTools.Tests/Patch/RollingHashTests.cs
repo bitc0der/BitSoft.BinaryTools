69e4d0b84fcb7d509f6006c498a4530d33236b24
bitc0der <59016822+bitc0der@users.noreply.github.com>
2025-11-30 19:48:08 +0700

    Improve hash

---
+++ src/BitSoft.BinaryTools.Tests/Patch/RollingHashTests.cs
@@ -0,0 +1,49 @@
+using BitSoft.BinaryTools.Patch;
+
+namespace BitSoft.BinaryTools.Tests.Patch;
+
+using System;
+
+[TestFixture]
+public class RollingHashTests
+{
+    [TestCase(1024, 32)]
+    [TestCase(1024, 64)]
+    [TestCase(1024, 128)]
+    [TestCase(1024, 256)]
+    public void Should_CalculateHash(int bufferLength, int bufferSize)
+    {
+        // Arrange
+        var buffer = new byte[bufferLength];
+
+        Random.Shared.NextBytes(buffer);
+
+        // Act & Assert
+        var initialSpan = buffer.AsSpan(start: 0, length: bufferSize);
+        var rollingHash = RollingHash.Create(initialSpan);
+        var gRollingHash = new Adler32RollingHash(bufferSize);
+        gRollingHash.CalculateInitialHash(initialSpan);
+
+        for (var i = 0; i < bufferLength - bufferSize; i++)
+        {
+            var span = buffer.AsSpan(start: i, length: bufferSize);
+
+            var spanHash = RollingHash.Create(span);
+
+            Assert.That(
+                actual: rollingHash.GetChecksum(),
+                expression: Is.EqualTo(spanHash.GetChecksum()),
+                message: $"Failed as position '{i}'"
+            );
+
+            if (i < bufferLength - bufferSize - 1)
+            {
+                var oldByte = buffer[i];
+                var newByte = buffer[i + bufferSize];
+
+                rollingHash.Update(removed: oldByte, added: newByte);
+                gRollingHash.Roll(oldByte, newByte);
+            }
+        }
+    }
+}
--- src/BitSoft.BinaryTools/Patch/RollingHash.cs
+++ src/BitSoft.BinaryTools/Patch/RollingHash.cs
@@ -8,9 +8,9 @@ public struct RollingHash
 
     private uint _a;
     private uint _b;
-    private readonly uint _length;
+    private readonly long _length;
 
-    private RollingHash(uint a, uint b, uint length)
+    private RollingHash(uint a, uint b, int length)
     {
         _a = a;
         _b = b;
@@ -22,21 +22,22 @@ public struct RollingHash
         uint a = 1;
         uint b = 0;
 
-        for (var i = 0; i < data.Length; i++)
+        foreach (var value in data)
         {
-            var value = data[i];
-
             a = (a + value) % Base;
             b = (b + a) % Base;
         }
 
-        return new RollingHash(a: a, b: b, length: (uint)data.Length);
+        return new RollingHash(a: a, b: b, length: data.Length);
     }
 
     public void Update(byte removed, byte added)
     {
         _a = (_a - removed + added) % Base;
-        _b = (_b - _length * removed + _a - 1) % Base;
+
+        var temp = _b - _length * removed + _a - 1;
+
+        _b = (uint)((temp % Base + Base) % Base);
     }
 
     public uint GetChecksum() => (_b << 16) | _a;
